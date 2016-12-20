f972d57972ac7bf67caa39b11236af7fddc79c4a
薛明星 <xuemingxingxmx@sina.cn>
2016-12-20 22:04:33 +0800

    初始化项目

---
+++ MyTestDemo/MyTestDemoTests/MyTestDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  MyTestDemoTests.m
+//  MyTestDemoTests
+//
+//  Created by 薛明星 on 16/12/20.
+//  Copyright © 2016年 xuemingxing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyTestDemoTests : XCTestCase
+
+@end
+
+@implementation MyTestDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
